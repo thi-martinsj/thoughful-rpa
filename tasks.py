d45410cd7ede1825f5fccb62053455d606a17a23
Thiago Martins <thiagomartins03@hotmail.com>
2024-08-19 11:22:53 -0300

    refactor: change tasks entrypoint

--- tasks.py
+++ tasks.py
@@ -2,9 +2,9 @@ import json
 
 from robocorp.tasks import task
 
-from .application.news_scrapper import NewsScrapper
-from .infrastructure.browsers.chrome_browser import ChromeBrowser
-from .infrastructure.repositories.ap_news_repository import APNewsRepository
+from src.application.news_scrapper import NewsScrapper
+from src.infrastructure.browsers.chrome_browser import ChromeBrowser
+from src.infrastructure.repositories.ap_news_repository import APNewsRepository
 
 
 @task
